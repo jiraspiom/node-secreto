119cdaf2db77d0d7cc0af0e33860198f0148191d
1gilmar <1gilmar@gmail.com>
2020-03-07 20:18:43 -0300

    bug de permiter postar comentario em branco criado mensaguem aletoria para o usuario

--- rotas/index.js
+++ rotas/index.js
@@ -5,17 +5,18 @@ require("../models/Segredo")
 const Segredo = mongoose.model("segredo")
 
 rotas.get("/", (req, res) => {
-   // updateCor()
+    // updateCor()
     Segredo.find().limit(88).sort({ "dataAt": 'desc' }).then((segredos) => {
-        res.render("index", { segredos: segredos })
+        res.render("index", { segredos: segredos, mensagem: mensagem()})
     }).catch((erro) => {
         res.send("erro ao buscar registro" + erro)
     })
 })
 rotas.post("/", (req, res) => {
+    //if (req.body.segredo || typeof req.body.segredo != undefined || req.body.segredo != null){
     if (req.body.segredo != undefined) {
         const novo = {
-            segredo: req.body.segredo,
+            segredo: req.body.segredo.trimLeft(),
             cor: cor(),
             dataAt: Date.now()
         }
@@ -49,18 +50,36 @@ function cor() {
 }
 
 //Funcao para atualizar os campos que não estava colorido
-function updateCor(){
-    Segredo.find({cor: undefined}).then((segredos)=>{
+function updateCor() {
+    Segredo.find({ cor: undefined }).then((segredos) => {
         segredos.forEach(segredo => {
             segredo.cor = cor()
-            segredo.save().then(()=>{
+            segredo.save().then(() => {
                 console.log("salvo com sucesso")
-            }).catch((erro)=>{
+            }).catch((erro) => {
                 console.log("erro ao salvar")
             })
 
         });
     })
+}
+
+function mensagem() {
+    const cores = [
+        "Vamos escrever alguma abobrinha aqui pessoal",
+        "Pode escrever, não doi...",
+        "Campo deve ser preenchido",
+        "Assim não da né, escreva ai uai.",
+        "Oxi, você não vai com minha cara, cade a mensaguem?",
+        "Ops, esqueceu da mensaguem",
+        "Parabens, só que você tem de escrever alguma coisa aqui.",
+        "Faltou a mensaguem",
+        "Não vou postar por que você não escreveu nada",
+        "Mensagem aqui, please!",
+        "Sem mensaguem não da pra postar",
+        "Verdade, não minto, mas mensaguem deve ser escrita nesse campo."
+    ]
+    return cores[Math.floor(Math.random() * cores.length)]
 }
 
 module.exports = rotas
